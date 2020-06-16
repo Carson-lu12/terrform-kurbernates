18b681b9fb3eb8465b72563ee994136fa7f59b8a
Lu Tianzhang <lutianzhang@Lus-MacBook-Pro.local>
2020-06-16 22:15:03 +0800

    kurbernets provision

---
+++ iac/main.tf
@@ -0,0 +1,43 @@
+#Project resources
+resource "google_container_cluster" "default" {
+  name = "${var.name}}"
+  project = "${var.project}"
+  description = "Demo GKE cluster"
+  location = "${var.location}"
+
+#default remove node pool
+remove_default_node_pool = true
+initial_node_count = "${var.initial_node_count}"
+
+#Authentication, if leave blank, no creditional is required
+master_auth {
+  username = ""
+  password = ""
+
+  clinet_certification_config {
+    issue_client_certification = false
+  }
+ }
+}
+
+resource "google_container_node_pool" "default" {
+  name = "${var.name}-node-pool"
+  project = "${var.project}"
+  location = "${var.location}"
+  cluster = "${google_container_cluster.default.name}"
+  node_count = 1
+
+  node_config {
+    #Something like spot instance in aws
+    preemptible = true
+    machine_type = "${var.machine_type}"
+
+    metadata = {
+      disable-legacy-endpoint = "true"
+    }
+    oauth_scopes = [
+    "https://www.googleapis.com/auth/logging.write",
+    "https://www.gooogleapis.com/auth/monitoring",
+    ]
+  }
+}
---
+++ iac/variables.tf
@@ -0,0 +1,18 @@
+variable "name" {
+  default = "demo-cluster"
+}
+variable "project" {
+  default = "myproject-280503"
+}
+
+variable "lcation" {
+  default = "asia-southeast1"
+}
+
+variable "initial_node_count" {
+  default = 1
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+}
